947697e128f8daaf7b8abf6262dc7c73e53d213f
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2023-02-21 08:14:52 +0000

    first commit

---
+++ dailyChallengeWeek8Day4/src/main/java/di/atos/dailychallengeweek8day4/DailyChallengeWeek8Day4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package di.atos.dailychallengeweek8day4;
+
+/**
+ *
+ * @author ezekielkouassi
+ */
+public class DailyChallengeWeek8Day4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
